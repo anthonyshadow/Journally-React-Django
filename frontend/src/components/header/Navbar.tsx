6cd9af2d7cc991db4039bde3b8d69e3284415563
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-03-07 12:57:55 -0500

    setup antd header

---
+++ frontend/src/components/header/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./Navbar.css";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const Navbar = () => {
+
+    const { Header } = Layout;
+
+    return(
+    <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Layout style={{ padding: "0 24px 24px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+          </Layout>
+        </Layout>
+      </Layout>
+    )
+}
+
+export default Navbar
